84e24e74ada1403d0dabff16a146712b33e764da
Kevin Buffardi <kbuffardi@csuchico.edu>
2020-10-08 12:11:46 -0700

    Starter code for 111

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
+  vector <double> receipts{2.50, 0.99, 15.25};
+  double additional;
+
+  do{
+    cout << "There are " << receipts.size() << " purchases.\n";
+    
+    cout << "Enter new purchase (or -1 to quit):\n";
+    cin >> additional;
+
+    if( additional >= 0 )
+    {
+      receipts.push_back( additional );
+    }
+  }while( additional >= 0 );
   
   return 0;
 }
